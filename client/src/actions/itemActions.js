29d178932affa26032561e92121e818f0526d7e3
Diego Speroni <diegosperoni1@gmail.com>
2018-07-09 10:00:35 -0300

    Some fixes

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,9 +1,14 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types'
+import axios from 'axios'
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types'
 
-export const getItems = () => {
-  return {
-    type: GET_ITEMS
-  }
+export const getItems = () => dispatch => {
+  dispatch(setItemsLoading())
+  axios
+    .get('/api/items')
+    .then(res => dispatch({
+      type: GET_ITEMS,
+      payload: res.data
+    }))
 }
 
 export const addItem = item => {
@@ -12,9 +17,16 @@ export const addItem = item => {
     payload: item
   }
 }
+
 export const deleteItem = id => {
   return {
     type: DELETE_ITEM,
     payload: id
+  }
+}
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING
   }
 }
